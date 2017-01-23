dbeac83ff68c4a6f4872faf1735106be7d2db874
Kenan Millet <KVMillet@Gmail.com>
2017-01-22 21:41:17 -0500

    Starting Mk. V from scratch. Let's see how this turns out...

--- DigiLoader MkV/DigiLoader MkV/Algorithm.h
+++ DigiLoader MkV/DigiLoader MkV/Algorithm.h
@@ -1,99 +1,392 @@
 #pragma once
-#include "BaseAlgorithm.h"
 
-template<class Ret, class... Args>
-class Algorithm : public BaseAlgorithm
+//Main Template Version
+template<class Ret = void, class... Args>
+class Algorithm
 {
 public:
-	Algorithm();
-
-	virtual Ret run(Args&...) = 0;
-	Ret operator()(Args&...);
-
-	Algorithm<Ret, Args...>& invoke(void**, size_t) final;
-	Ret result() const;
-	operator Ret() const;
-private:
-	Ret* retValue;
+	Algorithm() = default;
+	virtual Ret operator()(Args...) = 0;
+	virtual operator bool() const;
+	virtual ~Algorithm() = default;
 };
 
-template<class Ret>
-class Algorithm<Ret> : public BaseAlgorithm
+//Specialization for Function Pointers
+template<class Ret, class... Args>
+class Algorithm<Ret(*)(Args...)> : public Algorithm<Ret, Args...>
 {
 public:
-	Algorithm();
+	typedef Ret(*CallType)(Args...);
+	Algorithm(CallType = doNothing);
 
-	virtual Ret run() = 0;
-	Ret operator()();
+	Algorithm(const Algorithm<CallType>&) = default;
+	Algorithm(Algorithm<CallType>&&) = default;
+	Algorithm<Ret(*)(Args...)>& operator=(const Algorithm<CallType>&) = default;
+	Algorithm<Ret(*)(Args...)>& operator=(Algorithm<CallType>&&) = default;
 
-	Algorithm<Ret>& invoke(void**, size_t) final;
-	Ret result() const;
-	operator Ret() const;
+	Algorithm<Ret(*)(Args...)>& operator=(CallType);
+
+	virtual ~Algorithm() = default;
+
+	Ret operator()(Args...);
+	operator bool() const;
 private:
-	Ret* retValue;
+	Ret(*fn)(Args...);
+	static Ret(*doNothing)(Args...);
+};
+//Specialization for Function Pointers returning void
+template<class... Args>
+class Algorithm<void(*)(Args...)> : public Algorithm<void, Args...>
+{
+public:
+	typedef void(*CallType)(Args...);
+	Algorithm(CallType = doNothing);
+
+	Algorithm(const Algorithm<CallType>&) = default;
+	Algorithm(Algorithm<CallType>&&) = default;
+	Algorithm<void(*)(Args...)>& operator=(const Algorithm<CallType>&) = default;
+	Algorithm<void(*)(Args...)>& operator=(Algorithm<CallType>&&) = default;
+
+	Algorithm<void(*)(Args...)>& operator=(CallType);
+
+	virtual ~Algorithm() = default;
+
+	void operator()(Args...);
+	operator bool() const;
+private:
+	void(*fn)(Args...);
+	static void(*doNothing)(Args...);
 };
 
-template<class Ret, class ...Args>
-inline Algorithm<Ret, Args...>::Algorithm()
-	: retValue(nullptr)
+//Specialization for Lambda Functions
+template<class Ret, class... Args>
+class Algorithm<Ret(Args...)> : public Algorithm<Ret, Args...>
 {
+public:
+	typedef Ret(*CallType)(Args...);
+	Algorithm();
+	template<class Lambda> Algorithm(const Lambda& lambda);
+	
+	Algorithm(const Algorithm<Ret(Args...)>&);
+	Algorithm(Algorithm<Ret(Args...)>&&);
+	Algorithm<Ret(Args...)>& operator=(const Algorithm<Ret(Args...)>&);
+	Algorithm<Ret(Args...)>& operator=(Algorithm<Ret(Args...)>&&);
+	
+	template<class Lambda> Algorithm<Ret(Args...)>& operator=(const Lambda& lambda);
+
+	virtual ~Algorithm();
+
+	Ret operator()(Args...);
+	operator bool() const;
+private:
+	void* lambda;
+	void(*delLambda)(void*);
+	void*(*copyLambda)(void*);
+	Ret(*applyLambda)(void*, Args...);
+	bool is_good;
+};
+//Specialization for Lambda Functions returning void
+template<class... Args>
+class Algorithm<void(Args...)> : public Algorithm<void, Args...>
+{
+public:
+	typedef void(*CallType)(Args...);
+	Algorithm();
+	template<class Lambda> Algorithm(const Lambda& lambda);
+	
+	Algorithm(const Algorithm<void(Args...)>&);
+	Algorithm(Algorithm<void(Args...)>&&);
+	Algorithm<void(Args...)>& operator=(const Algorithm<void(Args...)>&);
+	Algorithm<void(Args...)>& operator=(Algorithm<void(Args...)>&&);
+
+	template<class Lambda> Algorithm<void(Args...)>& operator=(const Lambda& lambda);
+
+	virtual ~Algorithm();
+	
+	void operator()(Args...);
+	operator bool() const;
+private:
+	void* lambda;
+	void(*delLambda)(void*);
+	void*(*copyLambda)(void*);
+	void(*applyLambda)(void*, Args...);
+	bool is_good;
+};
+
+
+
+//////////////////////
+//Member Definitions//
+//////////////////////
+
+//Main Template Version
+template<class Ret, class... Args>
+inline Algorithm<Ret, Args...>::operator bool() const
+{
+	return true;
 }
 
-template<class Ret>
-inline Algorithm<Ret>::Algorithm()
-	: retValue(nullptr)
+
+
+//Specialization for Function Pointers
+template<class Ret, class... Args>
+inline Algorithm<Ret(*)(Args...)>::Algorithm(CallType fn)
+	: fn(fn)
 {
 }
 
 template<class Ret, class... Args>
-inline Ret Algorithm<Ret, Args...>::operator()(Args&... args)
+inline Algorithm<Ret(*)(Args...)>& Algorithm<Ret(*)(Args...)>::operator=(CallType fn)
 {
-	return run(args...);
-}
-
-template<class Ret>
-inline Ret Algorithm<Ret>::operator()()
-{
-	return run();
+	this->fn = fn;
 }
 
 template<class Ret, class... Args>
-inline Algorithm<Ret, Args...>& Algorithm<Ret, Args...>::invoke(void** args, size_t size)
+inline Ret Algorithm<Ret(*)(Args...)>::operator()(Args... args)
 {
-	auto next = [](void**& a) {return *a++; };
-	delete retValue;
-	retValue = new Ret(run(*((Args*)next(args))...) );
+	return fn(args...);
+}
+
+template<class Ret, class... Args>
+inline Algorithm<Ret(*)(Args...)>::operator bool() const
+{
+	return (fn == doNothing);
+}
+
+template<class Ret, class... Args>
+Ret(*Algorithm<Ret(*)(Args...)>::doNothing)(Args...) = [](Args...) { return Ret(); };
+//Specialization for Function Pointers returning void
+template<class... Args>
+inline Algorithm<void(*)(Args...)>::Algorithm(CallType fn)
+	: fn(fn)
+{
+}
+
+template<class... Args>
+inline Algorithm<void(*)(Args...)>& Algorithm<void(*)(Args...)>::operator=(CallType fn)
+{
+	this->fn = fn;
+}
+
+template<class... Args>
+inline void Algorithm<void(*)(Args...)>::operator()(Args... args)
+{
+	fn(args...);
+}
+
+template<class... Args>
+inline Algorithm<void(*)(Args...)>::operator bool() const
+{
+	return (fn == doNothing);
+}
+
+template<class... Args>
+void(*Algorithm<void(*)(Args...)>::doNothing)(Args...) = [](Args...) {};
+
+
+
+//Specialization for Lambda Functions
+template<class Ret, class... Args>
+inline Algorithm<Ret(Args...)>::Algorithm()
+	: is_good(false)
+{
+	auto placeholder = [](Args...) -> Ret { return Ret(); };
+	typedef decltype(placeholder) PL;
+	lambda = new PL(placeholder);
+	delLambda = [](void* l) { delete ((PL*) l); };
+	copyLambda = [](void* l) -> void* { new PL(*((PL*) l)); };
+	applyLambda = [](void* l, Args... args) -> Ret { return ((PL*) l)->operator()(args...); };
+}
+
+template<class Ret, class... Args>
+template<class Lambda>
+inline Algorithm<Ret(Args...)>::Algorithm(const Lambda& lambda)
+	: is_good(true)
+{
+	this->lambda = new Lambda(lambda);
+	delLambda = [](void* l) { delete ((Lambda*) l); };
+	copyLambda = [](void* l) -> void* { return new Lambda(*((Lambda*) l)); };
+	applyLambda = [](void* l, Args... args) -> Ret { return ((Lambda*) l)->operator()(args...); };
+}
+
+template<class Ret, class... Args>
+inline Algorithm<Ret(Args...)>::Algorithm(const Algorithm<Ret(Args...)>& obj)
+	: lambda(obj.copyLambda(obj.lambda)),
+	delLambda(obj.delLambda),
+	copyLambda(obj.copyLambda),
+	applyLambda(obj.applyLambda),
+	is_good(obj.is_good)
+{
+}
+
+template<class Ret, class... Args>
+inline Algorithm<Ret(Args...)>::Algorithm(Algorithm<Ret(Args...)>&& tmp)
+	: lambda(tmp.lambda),
+	delLambda(tmp.delLambda),
+	copyLambda(tmp.copyLambda),
+	applyLambda(tmp.applyLambda),
+	is_good(tmp.is_good)
+{
+	tmp.delLambda = [](void*) {};
+}
+
+template<class Ret, class... Args>
+inline Algorithm<Ret(Args...)>& Algorithm<Ret(Args...)>::operator=(const Algorithm<Ret(Args...)>& obj)
+{
+	delLambda(lambda);
+	lambda = obj.copyLambda(obj.lambda);
+	delLambda = obj.delLambda;
+	copyLambda = obj.copyLambda;
+	applyLambda = obj.applyLambda;
+	is_good = obj.is_good;
 	return *this;
 }
 
-template<class Ret>
-inline Algorithm<Ret>& Algorithm<Ret>::invoke(void** args, size_t size)
+template<class Ret, class... Args>
+inline Algorithm<Ret(Args...)>& Algorithm<Ret(Args...)>::operator=(Algorithm<Ret(Args...)>&& tmp)
 {
-	delete retValue;
-	retValue = new Ret(run());
+	void* oldLambda = lambda;
+	void(*oldDel)(void*) = delLambda;
+	lambda = tmp.lambda;
+	delLambda = tmp.delLambda;
+	tmp.lambda = oldLambda;
+	tmp.delLambda = oldDel;
+	copyLambda = tmp.copyLambda;
+	applyLambda = tmp.applyLambda;
+	is_good = tmp.is_good;
 	return *this;
 }
 
 template<class Ret, class... Args>
-inline Ret Algorithm<Ret, Args...>::result() const
+template<class Lambda>
+inline Algorithm<Ret(Args...)>& Algorithm<Ret(Args...)>::operator=(const Lambda& lambda)
 {
-	return *retValue;
-}
-
-template<class Ret>
-inline Ret Algorithm<Ret>::result() const
-{
-	return *retValue;
+	delLambda(this->lambda);
+	this->lambda = new Lambda(lambda);
+	delLambda = [](void* l) { delete ((Lambda*) l); };
+	copyLambda = [](void* l) -> void* { new Lambda(*((Lambda*) l)); };
+	applyLambda = [](void* l, Args... args) -> Ret { return ((Lambda*) l)->operator()(args...); };
+	is_good = true;
+	return *this;
 }
 
 template<class Ret, class... Args>
-inline Algorithm<Ret, Args...>::operator Ret() const
+inline Algorithm<Ret(Args...)>::~Algorithm()
 {
-	return result();
+	delLambda(lambda);
 }
 
-template<class Ret>
-inline Algorithm<Ret>::operator Ret() const
+template<class Ret, class... Args>
+inline Ret Algorithm<Ret(Args...)>::operator()(Args... args)
 {
-	return result();
+	return applyLambda(lambda, args...);
+}
+
+template<class Ret, class... Args>
+inline Algorithm<Ret(Args...)>::operator bool() const
+{
+	return is_good;
+}
+
+//Specialization for Lambda Functions returning void
+template<class... Args>
+inline Algorithm<void(Args...)>::Algorithm()
+	: is_good(false)
+{
+	auto placeholder = [](Args...) {};
+	typedef decltype(placeholder) PL;
+	lambda = new PL(placeholder);
+	delLambda = [](void* l) { delete ((PL*) l); };
+	copyLambda = [](void* l) -> void* { new PL(*((PL*) l)); };
+	applyLambda = [](void* l, Args... args) { ((PL*) l)->operator()(args...); };
+}
+
+template<class... Args>
+template<class Lambda>
+inline Algorithm<void(Args...)>::Algorithm(const Lambda& lambda)
+	: is_good(true)
+{
+	this->lambda = new Lambda(lambda);
+	delLambda = [](void* l) { delete ((Lambda*) l); };
+	copyLambda = [](void* l) -> void* { return new Lambda(*((Lambda*) l)); };
+	applyLambda = [](void* l, Args... args) { ((Lambda*) l)->operator()(args...); };
+}
+
+template<class... Args>
+inline Algorithm<void(Args...)>::Algorithm(const Algorithm<void(Args...)>& obj)
+	: lambda(obj.copyLambda(obj.lambda)),
+	delLambda(obj.delLambda),
+	copyLambda(obj.copyLambda),
+	applyLambda(obj.applyLambda),
+	is_good(obj.is_good)
+{
+}
+
+template<class... Args>
+inline Algorithm<void(Args...)>::Algorithm(Algorithm<void(Args...)>&& tmp)
+	: lambda(tmp.lambda),
+	delLambda(tmp.delLambda),
+	copyLambda(tmp.copyLambda),
+	applyLambda(tmp.applyLambda),
+	is_good(tmp.is_good)
+{
+	tmp.delLambda = [](void*) {};
+}
+
+template<class... Args>
+inline Algorithm<void(Args...)>& Algorithm<void(Args...)>::operator=(const Algorithm<void(Args...)>& obj)
+{
+	delLambda(lambda);
+	lambda = obj.copyLambda(obj.lambda);
+	delLambda = obj.delLambda;
+	copyLambda = obj.copyLambda;
+	applyLambda = obj.applyLambda;
+	is_good = obj.is_good;
+	return *this;
+}
+
+template<class... Args>
+inline Algorithm<void(Args...)>& Algorithm<void(Args...)>::operator=(Algorithm<void(Args...)>&& tmp)
+{
+	void* oldLambda = lambda;
+	void(*oldDel)(void*) = delLambda;
+	lambda = tmp.lambda;
+	delLambda = tmp.delLambda;
+	tmp.lambda = oldLambda;
+	tmp.delLambda = oldDel;
+	copyLambda = tmp.copyLambda;
+	applyLambda = tmp.applyLambda;
+	is_good = tmp.is_good;
+	return *this;
+}
+
+template<class... Args>
+template<class Lambda>
+inline Algorithm<void(Args...)>& Algorithm<void(Args...)>::operator=(const Lambda& lambda)
+{
+	this->delLambda(this->lambda);
+	this->lambda = new Lambda(lambda);
+	this->delLambda = [](void* l) { delete ((Lambda*) l); };
+	this->copyLambda = [](void* l) -> void* { new Lambda(*((Lambda*) l)); };
+	this->applyLambda = [](void* l, Args... args) { ((Lambda*) l)->operator()(args...); };
+	this->is_good = true;
+	return *this;
+}
+
+template<class... Args>
+inline Algorithm<void(Args...)>::~Algorithm()
+{
+	delLambda(lambda);
+}
+
+template<class... Args>
+inline void Algorithm<void(Args...)>::operator()(Args... args)
+{
+	applyLambda(lambda, args...);
+}
+
+template<class... Args>
+inline Algorithm<void(Args...)>::operator bool() const
+{
+	return is_good;
 }
--- DigiLoader MkV/DigiLoader MkV/DigiLoader MkV.cpp
+++ DigiLoader MkV/DigiLoader MkV/DigiLoader MkV.cpp
@@ -1,26 +1,80 @@
 // DigiLoader MkV.cpp : Defines the entry point for the console application.
 //
-#include "DynamicAlgorithm.h"
+#include "Algorithm.h"
 #include "stdafx.h"
 
 #include <iostream>
 
-int test(int) { return 1; }
-
 using namespace std;
 
-class algo : public Algorithm<int, int>
+struct doA : public Algorithm<float>
 {
-public:
-	int run(int&) { return 2; }
+	float operator()() { return 3.14f; }
+};
+struct doB : public Algorithm<float, float>
+{
+	float operator()(float k) { return k * 3.14f; }
+};
+struct doC : public Algorithm<void, float>
+{
+	void operator()(float) {}
+};
+struct doD : public Algorithm<>
+{
+	void operator()() {}
 };
 
+void doE()
+{
+
+}
+void doF(int)
+{
+
+}
+float doG()
+{
+	return 1.618f;
+}
+int doH(int x)
+{
+	return x;
+}
 
 int main()
 {
-	auto d = DynamicAlgorithm<decltype(&test), int, int>(&test);
-	algo a;
-	int x = 0;
+	int answer = 42;
+	auto doI = []() {};
+	auto doJ = [](int) {};
+	auto doK = [](int x) { return x + 1; };
+	auto doL = [answer]() { return answer; };
+
+	doA a;
+	doB b;
+	doC c;
+	doD d;
+	Algorithm<void(*)()> e(&doE);
+	Algorithm<void(*)(int)> f(&doF);
+	Algorithm<float(*)()> g(&doG);
+	Algorithm<int(*)(int)> h(&doH);
+	Algorithm<void()> i(doI);
+	Algorithm<void(int)> j(doJ);
+	Algorithm<int(int)> k(doK);
+	Algorithm<int()> l(doL);
+
+	cout << a() << endl;
+	cout << b(3.14f) << endl;
+	c(1.618f);
+	d();
+	e();
+	f(24);
+	cout << g() << endl;
+	cout << h(12) << endl;
+	i();
+	j(2);
+	cout << k(6) << endl;
+	cout << l() << endl;
+
 	cin.ignore();
 	return 0;
 }
